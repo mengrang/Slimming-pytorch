69f25aac0e6d1d9677cb5987c44a15e17f43a01f
ame <r_meng@zju.edu.cn>
2019-01-07 22:45:14 +0800

    db

--- models/new_resnet.py
+++ models/new_resnet.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
+import numpy as np
+import os
+import os.path as osp
+from real_prune import *
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
@@ -22,9 +26,6 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=True)
 
-def L1_penalty(var):
-    return torch.abs(var).sum()
-
 def BatchNorm2d_no_b(num_features, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True):
     bn = nn.BatchNorm2d(num_features, eps, momentum, affine, track_running_stats)
     bn.bias.requires_grad = False
@@ -33,12 +34,12 @@ def BatchNorm2d_no_b(num_features, eps=1e-5, momentum=0.1, affine=True, track_ru
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, index, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = BatchNorm2d_no_b(planes)
+        self.conv1 = conv3x3(inplanes, index[0], stride)
+        self.bn1 = BatchNorm2d_no_b(index[0])
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
+        self.conv2 = conv3x3(index[1], planes)
         self.bn2 = BatchNorm2d_no_b(planes)
         self.downsample = downsample
         self.stride = stride
@@ -66,15 +67,15 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, index, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, block_idx, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, index[0], kernel_size=1, bias=True)
-        self.bn1 = BatchNorm2d_no_b(planes)
-        self.conv2 = nn.Conv2d(index[0], index[1], kernel_size=3, stride=stride,
+        self.conv1 = nn.Conv2d(inplanes, block_idx[0], kernel_size=1, bias=True)
+        self.bn1 = BatchNorm2d_no_b(idx[0])
+        self.conv2 = nn.Conv2d(idx[0], block_idx[1], kernel_size=3, stride=stride,
                                padding=1, bias=True)
-        self.bn2 = BatchNorm2d_no_b(index[1])
-        self.conv3 = nn.Conv2d(block_index[1], index[2], kernel_size=1, bias=True)
-        self.bn3 = BatchNorm2d_no_b(index[2])
+        self.bn2 = BatchNorm2d_no_b(block_idx[1])
+        self.conv3 = nn.Conv2d(block_idx[1], planes * 4, kernel_size=1, bias=True)
+        self.bn3 = BatchNorm2d_no_b(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -103,20 +104,28 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000):
+    def __init__(self, block, layers, num_classes=1000, net_index=None):
         self.inplanes = 64
         super(ResNet, self).__init__()
+        if net_index = None:
+            ## Standard Resnet Structure
+            net_index = [
+                    [[64]], 
+                    [[[64, 64]]*layers[0]], 
+                    [[[128, 128]]*layers[1]], 
+                    [[[256, 256]]*layers[2]], 
+                    [[[512, 512]]*layers[3]]
+                    ]
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=True)
-        self.bn1 = BatchNorm2d_no_b(64)
+        self.bn1 = BatchNorm2d_no_b(net_index[0][0][0])
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.layer1 = self._make_layer(block, 64, layers[0], net_index[1])
+        self.layer2 = self._make_layer(block, 128, layers[1], net_index[2], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], net_index[3], stride=2)
+        # modify the layer 4 stride 
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -126,14 +135,11 @@ class ResNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1.)
                 m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, unit_index, blocks, stride=1):
+    def _make_layer(self, block, planes, blocks, layer_idx, stride=1):
         downsample = None
         
-        if stride != 1 or self.inplanes != unit_index[0]:
+        if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=True),
@@ -141,10 +147,10 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(self.inplanes, planes, layer_idx[0], stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(block(self.inplanes, planes, layer_idx[i]))
 
         return nn.Sequential(*layers)
 
@@ -152,22 +158,35 @@ class ResNet(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        co_layer = x
-        x = self.maxpool(x)       
+        x = self.maxpool(x)   
+
         x = self.layer1(x)
-        x = self.layer2(x)    
-        im = self.gap(self.relu1(x)).view(x.size(0), -1)
-        im = self.fc1(im)
+        fm1 = x
+        x = self.layer2(x)
+        fm2 = x
         x = self.layer3(x)
+        fm3 = x
         x = self.layer4(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = nn.Dropout(p=0.5)(x)
-        x = self.fc(x)
-        
-        return x
 
+        return x, (fm1, fm2, fm3)
 
+def slim_finetune(model, ckpt_dir, ratio):
+    model_dict = model.state_dict() 
+    ckpt = torch.load(osp.join(ckpt_dir, "ckpt.pth"))
+    ckpt_dict = ckpt['state_dicts'][0]
+    bn_w_dict = bn_weights(ckpt_dict)
+    bnw_index, slim_bnw_dict = slim_bnws(bn_w_dict, ratio)
+    model_dict.update(slim_bnw_dict)
+    return model_dict
+    # bn_b_dict = dict()
+    # for k, v in model_dict.items():
+    #     for i in k.split('.'):
+    #         if (i == 'bn1' or i == 'bn2' or i == 'bn3') and k.endswith('bias'):
+    #             bn_b_dict[k] = v
+    # pre_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and k not in bn_b_dict}
+    # model_dict.update(pre_dict)
+    # return model_dict
+    
 def resnet18(pretrained=False, **kwargs):
     """Constructs a ResNet-18 model.
 
@@ -199,8 +218,9 @@ def resnet50(pretrained=False, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    model.load_state_dict(slim_finetune(model, ckpt_dir, ratio))
     if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+        model.load_state_dict(removed_dict(model_urls['resnet50'], model))
     return model
 
 
---
+++ real_prune.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+import numpy as np
+import os
+import torch.nn as nn
+import torch
+import torch.nn.functional as f
+import torch.utils.data
+from torch.nn import DataParallel
+from datetime import datetime
+from config import *
+from distill import teacher, student
+from dataset import dataset
+from utils import *
+import collections
+
+def mask(var, thr):
+    return list(var.cpu().abs().gt(thr).float().numpy())
+
+def bn_channels(var, thr):
+    var = mask(var, thr)
+    return sum(var)
+
+def bn_weights(state_dict):
+    bn_w_dict = OrderedDict()
+    for k, v in state_dict.items():
+        for i in k.split('.'):
+            if (i == 'bn1' or i == 'bn2') and k.endswith('weight'):
+                bn_w_dict[k] = v
+    return bn_w_dict
+
+def slim_channels(bn_w_dict, layers, ratio, model='ResNet50'):
+    block_clust = []
+    layer_clust = []
+    bnw_state = bn_state(bn_w_dict, ratio)
+    if model == 'ResNet50':
+        net_channels = [
+                [[64]], 
+                [[64, 64]]*layers[0], 
+                [[128, 128]]*layers[1], 
+                [[256, 256]]*layers[2], 
+                [[512, 512]]*layers[3]]           
+    flat_channels = [int(bn_channels(v, min(float(bnw_state[k][-1]), 0.05))) for k, v in bn_w_dict.items()]
+    net_channels[0][0] = [flat_channels[0]]
+    for i in range(1, len(flat_channels), 2):
+        block_clust.append(flat_channels[i:i+2])
+    layer_clust = [block_clust[:layers[0]]]
+    for i in range(1, len(layers)): 
+        layer_clust.append(block_clust[sum(layers[:i]):sum(layers[:i+1])])
+    net_channels[1:] = layer_clust
+    return net_channels
+
+def slim_bnws(bn_w_dict, ratio):
+    bnw_index = OrderedDict()
+    bnw_state = bn_state(bn_w_dict, ratio)    
+    bnw_index = {k:np.argwhere(v.abs().cpu().numpy() > min(float(bnw_state[k][-1]), 0.05))
+                for k,v in bn_w_dict.items()}
+    pruned_bn_w_dict = {k:v[bnw_index[k]] for k,v in bn_w_dict.items()}
+    return bnw_index, pruned_bn_w_dict
+    
+def threshold_adap(arr, ratio):
+    arr.sort()
+    num = arr.size
+    elm = arr[int(num*(1-ratio))]
+    return elm
+
+def slim_statistic(bn_w_dict, layers, ratio, model='ResNet50'):
+    C_slim_ratio = []
+    bnw_state = bn_state(bn_w_dict, ratio)
+    if model == 'ResNet50':
+        flat_net_C = [c for layer in ResNet50_C for block in layer for c in block]
+    flat_slim_C = [bn_channels(v, min(float(bnw_state[k][-1]), 0.05)) for k, v in bn_w_dict.items()]
+    for p in zip(flat_slim_C, flat_net_C):
+        C_slim_ratio.append(round(p[0]/p[1], 4))
+    total_C_slim_ratio = sum(C_slim_ratio) / len(flat_net_C)
+
+    return C_slim_ratio, total_C_slim_ratio
+
+def bn_state(bn_w_dict, ratio):
+    bnw_state = OrderedDict()
+
+    bnw_state = {k:[v.abs().cpu().numpy().max(), 
+                v.abs().cpu().numpy().min(), 
+                v.abs().cpu().numpy().mean(), 
+                np.median(v.abs().cpu().numpy()),
+                threshold_adap(v.abs().cpu().numpy(), ratio)] for k,v in bn_w_dict.items()}
+    return bnw_state
+
+
+if __name__ == "__main__": 
+    ResNet50_LAYERS = [3, 4, 6, 3]
+    ResNet50_C = [
+            [[64]], 
+            [[64, 64]]*ResNet50_LAYERS[0], 
+            [[128, 128]]*ResNet50_LAYERS[1], 
+            [[256, 256]]*ResNet50_LAYERS[2], 
+            [[512, 512]]*ResNet50_LAYERS[3]
+            ]
+    ckpt = torch.load(osp.join("/home/zhangming/log/mr/train_slim/prune", "ckpt.pth"))
+    ckpt_dict = ckpt['state_dicts'][0]
+ 
+    bn_w_dict = bn_weights(ckpt_dict)
+
+    bnw_index, slim_bnw_dict = slim_bnws(bn_w_dict, 0.7)  
+    net_channels = slim_channels(bn_w_dict, ResNet50_LAYERS, 0.7, model='ResNet50')
+    print([
+        [[64]], 
+        [[64, 64]]*3, 
+        [[128, 128]]*4, 
+        [[256, 256]]*6, 
+        [[512, 512]]*3])
+    print(net_channels)
+    C_slim_ratio, total_C_slim_ratio = slim_statistic(bn_w_dict, ResNet50_LAYERS, 0.7, model='ResNet50')
+    print('Channels Slimming Ratio:\n{}'.format(C_slim_ratio))
+    print('Total Channels Slimming Ratio:\n{:4f}'.format(total_C_slim_ratio))
+    bnw_state = bn_state(bn_w_dict, 0.7)
+    for k,v in bnw_state.items():
+        print(k)
+        print(v[-1])
